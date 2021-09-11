fea8ccb79597ec5eee339fd0bc99258704c0f4a2
Felipe Nunes <felipenunesseixas@gmail.com>
2021-09-11 11:15:02 -0300

    Refactored the code and added the new grid and clear buttons.

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-
-function blackBg(event) {
-    event.target.style.background = "black";
-}
-
 const gridContainer = document.querySelector(".grid-container");
+const clearButton = document.querySelector(".clear");
+const sketchButton = document.querySelector(".sketch");
 let number = 16;
-for(let i = 0; i < (16**2); i++) {
-    let div = document.createElement("div");
-    gridContainer.appendChild(div);
-    div.addEventListener("mouseover", blackBg);
+
+function drawGrid(number) {
+    gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    for (let i = 0; i < (number**2); i++) {
+        let div = document.createElement("div");
+        gridContainer.appendChild(div);
+        div.addEventListener("mouseover", function(event){
+            event.target.style.background = "black";
+        });
+    }
 }
+
+function clearGrid(){
+    gridContainer.innerHTML = "";
+}
+
+function newGrid() {
+    let promptNumber = parseInt(prompt("Size of the new grid (limit of 70): "));
+    if (promptNumber > 70 || promptNumber < 1) {
+        clearGrid();
+        drawGrid(number);
+    }
+    else {
+        clearGrid();
+        drawGrid(promptNumber);
+        number = promptNumber;
+    }
+}
+
+clearButton.addEventListener("click", function() {
+    clearGrid();
+    drawGrid(number);
+})
+
+sketchButton.addEventListener("click", newGrid);
+
+drawGrid(number);
